1592b18c43b9cfe37391b599c2deeb0df72d56fd
Виктор Парыгин <paryginva@live.com>
2022-01-09 22:50:58 +0500

    Добавьте файлы проекта.

---
+++ WpfApp_Task9/MainWindow.xaml.cs
@@ -0,0 +1,150 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfApp_Task9
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            List<string> styles = new List<string>() { "Светлая тема", "Темная тема" };
+            styleBox.ItemsSource = styles;
+            styleBox.SelectionChanged += ThemeChange;
+            styleBox.SelectedIndex = 0;
+        }
+
+        private void ThemeChange(object sender, SelectionChangedEventArgs e)
+        {
+            int styleIndex = styleBox.SelectedIndex; 
+            Uri uri = new Uri("Light.xaml", UriKind.Relative); 
+            if (styleIndex == 1)
+            {
+                uri = new Uri("Dark.xaml", UriKind.Relative);
+            }
+            ResourceDictionary resource = Application.LoadComponent(uri) as ResourceDictionary;
+            Application.Current.Resources.Clear();
+            Application.Current.Resources.MergedDictionaries.Add(resource);
+        }
+
+
+        private void ComboBox_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            string fontName = ((sender as ComboBox).SelectedItem as string);
+            if (textBox != null)
+                textBox.FontFamily = new FontFamily(fontName);
+        }
+
+        private void ComboBox_SelectionChanged_1(object sender, SelectionChangedEventArgs e)
+        {
+            string fontSize = ((sender as ComboBox).SelectedItem as string);
+            if (textBox != null)
+                textBox.FontSize = Convert.ToDouble(fontSize);
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontWeight == FontWeights.Normal)
+            {
+                textBox.FontWeight = FontWeights.Bold;
+            }
+            else
+            {
+                textBox.FontWeight = FontWeights.Normal;
+            }
+
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox.FontStyle == FontStyles.Normal)
+            {
+                textBox.FontStyle = FontStyles.Italic;
+            }
+            else
+            {
+                textBox.FontStyle = FontStyles.Normal;
+            }
+        }
+
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            if (textBox.TextDecorations == null)
+            {
+                textBox.TextDecorations = TextDecorations.Underline;
+            }
+            else
+            {
+                textBox.TextDecorations = null;
+            }
+        }
+
+        private void RadioButton_Checked(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+            {
+                textBox.Foreground = styleBox.SelectedIndex == 1 ? Brushes.White : Brushes.Black;
+            }
+        }
+
+        private void RadioButton_Checked_1(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+                textBox.Foreground = Brushes.Red;
+        }
+
+        private void RadioButton_Checked_2(object sender, RoutedEventArgs e)
+        {
+            if (textBox != null)
+                textBox.Foreground = Brushes.Green;
+
+        }
+
+        private void OpenExecuted(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                textBox.Text = File.ReadAllText(openFileDialog.FileName);
+            }
+        }
+
+        private void SaveExecuted(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog saveFileDialog = new SaveFileDialog();
+            saveFileDialog.Filter = "Текстовые файлы (*.txt)|*.txt|Все файлы (*.*)|*.*";
+            if (saveFileDialog.ShowDialog() == true)
+            {
+                File.WriteAllText(saveFileDialog.FileName, textBox.Text);
+            }
+        }
+
+        private void ExitExecuted(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void NewExecuted(object sender, ExecutedRoutedEventArgs e)
+        {
+            // Здесь пока только заглушка //
+        }
+    }
+}
---
+++ WpfApp_Task9/Task7_Command.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace WpfApp_Task9
+{
+    class Task7_Command
+    {
+        public static RoutedUICommand Exit { get; set; }
+        public static RoutedUICommand LightTheme { get; set; }
+        public static RoutedUICommand DarkTheme { get; set; }
+        static Task7_Command()
+        {
+            InputGestureCollection inputs = new InputGestureCollection();
+            inputs.Add(new KeyGesture(Key.T, ModifierKeys.Control, "Ctrl+T"));
+            inputs.Add(new KeyGesture(Key.L, ModifierKeys.Control, "Ctrl+L"));
+            inputs.Add(new KeyGesture(Key.D, ModifierKeys.Control, "Ctrl+D"));
+            Exit = new RoutedUICommand("Выход", "Exit", typeof(Task7_Command), inputs);
+            LightTheme = new RoutedUICommand("Светлая", "LightTheme", typeof(Task7_Command), inputs);
+            DarkTheme = new RoutedUICommand("Темная", "DarkTheme", typeof(Task7_Command), inputs);
+        }
+    }
+}
